904f698d69026f87e46c7f2260291796fdd1f919
jiroor <nb-or25.d0j-pmf@hotmail.co.jp>
2015-09-21 10:19:16 +0900

    feat(voicetext): play recognized sentence by voicetext sound

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
+var fs = require('fs');
+var async = require('neo-async');
+
 var Julius = require('julius');
+var VoiceText = require('voicetext');
+var wav = require('wav');
+var Speaker = require('speaker');
 
-var grammar = new Julius.Grammar();
-// registry grammar
-grammar.add('おはようございます');
-grammar.add('こんにちは');
-grammar.add('おやすみなさい');
-grammar.compile(function(err, res) {
-  if (err) {
-    return;
-  }
+var grammar, voiceText, speaker, reader;
 
-  // create julius instance
+async.angelfall([
+  setting,
+  compile,
+  startJulius
+]);
+
+function setting(next) {
+  grammar = new Julius.Grammar();
+  // registry grammar
+  grammar.add('おはようございます');
+  grammar.add('こんにちは');
+  grammar.add('おやすみなさい');
+
+  voiceText = new VoiceText('<your api key>');
+
+  speaker = voiceText.speaker(voiceText.SPEAKER.SHOW);
+
+  next();
+}
+
+function compile(next) {
+  grammar.compile(next);
+}
+
+function startJulius(next) {
   var julius = new Julius(grammar.getJconf());
-  julius.on('result', resultFn);
+  julius.on('result', recognized);
   julius.start();
-});
+  next();
+}
 
-function resultFn(res) {
+function recognized(res) {
   console.log(res);
+  async.angelfall([
+    getWav.bind(null, res),
+    saveWav,
+    readyWav,
+    playWav
+  ]);
+}
+
+function getWav(res, next) {
+  speaker.speak(res, next);
+}
+
+function saveWav(buf, next) {
+  fs.writeFile('./tmp.wav', buf, 'binary', next);
+}
+
+function readyWav(next) {
+  var file = fs.createReadStream('./tmp.wav');
+  reader = new wav.Reader();
+  reader.on('format', next.bind(null, null));
+  file.pipe(reader);
+}
+
+function playWav(format, next) {
+  reader.pipe(new Speaker(format));
+  next();
 }
